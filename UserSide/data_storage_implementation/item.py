f98ca0453ce95cc47be649e5913074dd40dfdad9
TomerGat <107539126+TomerGat@users.noreply.github.com>
2023-06-26 04:16:16 +0300

    Delete UserSide directory

--- UserSide/data_storage_implementation/item.py
+++
@@ -1,9 +0,0 @@
-from util_functions import dict_to_str
-
-
-class Item:
-    def __init__(self, data: {}):
-        self.data = data
-
-    def get_data_size(self):
-        return len(dict_to_str(self.data))
